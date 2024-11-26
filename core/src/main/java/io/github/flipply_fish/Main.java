2bc6a9fe432c2a8c701d93f27cfa0a08f8439306
Ralph Terte <109493278+Jaserii@users.noreply.github.com>
2024-11-26 15:20:08 -0500

    Update main.java
    Started very basic groundwork to the game

--- core/src/main/java/io/github/flipply_fish/Main.java
+++ core/src/main/java/io/github/flipply_fish/Main.java
@@ -2,15 +2,77 @@ package io.github.flipply_fish;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
+    //  Game Resources
+    private Texture player;
+    private Sound bounce;
+    private SpriteBatch batch;
+    private FitViewport viewport;
+
+    //  Game resource filenames
+    private String playerTextureFile = "libgdx.png";
+
+    @Override
+    public void create() {
+        player = new Texture(playerTextureFile);
+        batch = new SpriteBatch();
+        viewport = new FitViewport(3, 6);
+    }
+
+    /**
+     * resize()
+     * Desc:    Adjust the viewport whenever the game window gets resized
+     * @param width the new width in pixels
+     * @param height the new height in pixels
+     */
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
+    }
+
+    @Override
+    public void render() {
+        // organize code into three methods
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        //  Draw sprites within this block
+        batch.begin();
+
+        batch.draw(player, 0, 0, 1, 1); // draw the bucket with width/height of 1 meter
+
+        batch.end();
+    }
+
+    /*
     private SpriteBatch batch;
     private Texture image;
+
 
     @Override
     public void create() {
@@ -31,4 +93,6 @@ public class Main extends ApplicationAdapter {
         batch.dispose();
         image.dispose();
     }
+
+     */
 }
