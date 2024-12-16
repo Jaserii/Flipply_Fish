700efbc170c30b363178bc5810ba68b1a20cd726
Ralph Terte <109493278+Jaserii@users.noreply.github.com>
2024-12-16 10:44:04 -0500

    Remove boilerplate code

--- core/src/main/java/io/github/flipply_fish/Main.java
+++ core/src/main/java/io/github/flipply_fish/Main.java
@@ -107,31 +107,4 @@ public class Main extends ApplicationAdapter {
         player.draw(batch);
         batch.end();
     }
-
-    /*
-    private SpriteBatch batch;
-    private Texture image;
-
-
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 800);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
-
-     */
 }
